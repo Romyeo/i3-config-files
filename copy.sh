b696656a61ba2c6bcbd70bd78f026a6a0b3eefe1
Romyeo <vincent.nocum@gmail.com>
2019-09-18 03:23:29 +0800

    Feat: added compton tryone config file

--- copy.sh
+++ copy.sh
@@ -4,3 +4,4 @@ sudo cp -r ~/.i3 ./i3
 sudo cp -r $CONFIG_PATH/polybar ./polybar
 sudo cp -r $CONFIG_PATH/termite ./termite
 sudo cp -r $CONFIG_PATH/cava ./cava
+sudo cp $CONFIG_PATH/compton.conf ./compton
